5aaa0fd634b2b6634ab92c9b694a4d619c105306
clementchuo <clementchuo@gmail.com>
2018-03-14 14:58:35 +0800

    edit config.php

--- connection/config.php
+++ connection/config.php
@@ -5,9 +5,5 @@ $dbuser = "root";
 $dbpass = "";
 $dbname = "lwe";
 
-mysql_connect($dbhost,$dbuser,$dbpass) or die('cannot connect to the server'); 
-mysql_select_db($dbname) or die('database selection problem');
 $con = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname) or die("Error " . mysqli_error($con));
-$db = new PDO('mysql:dbname=lwe;host=localhost','root','');
-
 ?>

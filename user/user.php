a895cd527ae4abedf919db5c28ac80619ede2c00
Samuel Hii <wosamuelh.t.o@hotmail.my>
2018-03-24 09:59:24 +0800

    update user profile

--- user/user.php
+++ user/user.php
@@ -1,3 +1,71 @@
+<?php
+
+require_once '../connection/config.php';
+session_start();
+$user_id = $_SESSION['user_id'];
+
+$query = "SELECT *
+           FROM users
+           WHERE user_id='$user_id'";
+$result = mysqli_query($con, $query);
+$results = mysqli_fetch_assoc($result);
+
+?>
+<?php
+if (isset($_POST['saveprofile']))
+{
+    $user_id = $_POST['user_id'];
+    $fname = $_POST['fname'];
+    $lname = $_POST['lname'];
+    $email = $_POST['email'];
+    $contact = $_POST['contact'];
+	$cp = $_POST['cp'];
+    $np = $_POST['np'];
+    $cnp = $_POST['cnp'];
+	
+    $result = mysqli_query($con, "UPDATE users SET fname='$fname', lname='$lname', contact='$contact', email='$email' WHERE user_id=$user_id") or die(mysqli_error($con));
+	
+	
+    if($cp != "" && $np!= "" && $cnp != "")
+    {
+        if($cp!=$np)
+        {
+            if($np==$cnp)
+            {
+				$password = $_POST['np'];
+				$password = password_hash($password, PASSWORD_DEFAULT); 
+                $result = mysqli_query($con, "UPDATE users SET password='$password' WHERE user_id=$user_id") or die(mysqli_error($con));
+            }
+            else
+            {
+                ?>
+                <script>
+                alert('Make sure new password and confirm password are match.');
+				window.location.href='main.php#user';
+                </script>
+                <?php
+            }
+        }
+        else
+        {
+            ?>
+            <script>
+            alert('Make sure new password different with old password.');
+            window.location.href='main.php#user';
+            </script>
+            <?php
+        }
+    }
+    ?>
+		<script>
+		alert('Successfully Update');
+        window.location.href='main.php#user';
+        </script>
+		<?php
+}
+  
+?>
+
 <div class="col-xs-12 col-md-12 col-lg-12">
     <h2 class="bigh2 pagetitle hidden-xs hidden-sm">My Profile</h2>
     
@@ -7,22 +75,23 @@
         <form class="form-inline left" action="user.php" method="post">
             <p>
                 <label for="fname">First name</label><br/>
-                <input type="text" class="formfield" id="fname" name="fname" required/>
-            </p>
+                <input type="text" class="formfield" id="fname" name="fname" value="<?php echo $results['fname']; ?>" required/>
+                <input type="hidden" name="user_id" value="<?php echo $user_id; ?>">
+			</p>
 
             <p>
                 <label for="lname">Last name</label><br/>
-                <input type="text" class="formfield" id="lname" name="lname" required/>
+                <input type="text" class="formfield" id="lname" name="lname" value="<?php echo $results['lname']; ?>" required/>
             </p>
 
             <p>
                 <label for="contact">Contact number </label><br/>
-                <input type="text" class="formfield" id="contact" name="contact" required/>
+                <input type="text" class="formfield" id="contact" name="contact" value="<?php echo $results['contact']; ?>" required/>
             </p>
 
             <p>
                 <label for="email">Email </label><br/>
-                <input type="email" class="formfield" id="email" name="email" required/>
+                <input type="email" class="formfield" id="email" name="email" value="<?php echo $results['email']; ?>" required/>
             </p>
 
             <p><button class="btn btn-link btntab" onclick="funcShowChgPassword()" id="btnShowChg">Click here to change password</button></p>
@@ -30,17 +99,17 @@
             <div id="chgPass">
                 <p>
                     <label for="password">Current Password </label><br/>
-                    <input type="password" class="formfield" id="curpassword" name="curpassword"/>
+                    <input type="password" class="formfield" id="cp" name="cp"value="<?php echo $results['password']; ?>" readonly required/>
                 </p>
 
                 <p>
                     <label for="password">New Password </label><br/>
-                    <input type="password" class="formfield" id="newpassword" name="newpassword" onkeyup="checkPass()" />
+                    <input type="password" class="formfield" id="np" name="np" onkeyup="checkPass()" />
                 </p>
 
                 <p>
                     <label for="repassword">Retype New Password </label><br/>
-                    <input type="password" class="formfield" id="renewpassword" name="renewpassword" onkeyup="checkPass()" /><br/>
+                    <input type="password" class="formfield" id="cnp" name="cnp" onkeyup="checkPass()" /><br/>
                     <span id="passno">Password does not match</span>
                 </p>
                 

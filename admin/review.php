4b4d5a562a9f0835ad274a44b656f67ffb433765
Samuel Hii <wosamuelh.t.o@hotmail.my>
2018-03-28 05:50:01 +0800

    display reciew

--- admin/review.php
+++ admin/review.php
@@ -1,3 +1,15 @@
+<?php
+require_once '../connection/config.php';
+session_start();
+
+$query = "SELECT *
+           FROM shipping s
+           JOIN users us
+           ON us.user_id = s.user_id";
+$result = mysqli_query($con, $query);
+
+
+?>
 <div class="col-xs-12 col-md-12 col-lg-12">
     <h2 class="bigh2 pagetitle hidden-xs hidden-sm">Reviews</h2>
     
@@ -5,7 +17,31 @@
     
     <!-- loop all reviews -->
     <div class="adreview left">
-        <h5></h5>
-        <p></p>
-    </div>
+                <table class="purchasetable">
+                    <tr class="center">
+                        <th>Customer</th>
+                        <th>Review</th>
+                    </tr>
+                    <?php 
+                        if(mysqli_num_rows($result) > 0)
+                        {
+                            while($row = mysqli_fetch_array($result))
+                            {
+                                ?>
+                                <tr>
+                                    <td><?php echo $row['fname']." ".$row['lname']; ?></td>
+                                    <td><?php echo $row['review']; ?></td>
+                                </tr>
+                                <?php
+                            }
+                        }else{
+                            ?>
+                                <tr>
+                                    <td colspan="4">No review submitted.</td>
+                                </tr>
+                            <?php
+                        }
+                    ?>
+                </table>
+        </div>
 </div>

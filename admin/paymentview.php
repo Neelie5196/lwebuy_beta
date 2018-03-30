4ffbc743a5d4920ce26a4a29840e1081960dc1bc
clementchuo <clementchuo@gmail.com>
2018-03-30 16:55:58 +0800

    done shipping declined and refund

--- admin/paymentview.php
+++ admin/paymentview.php
@@ -26,7 +26,7 @@ if(isset($_POST['updateordercode']))
 
         ?>
         <script>
-        alert('Order in proceed');
+        alert('Success to Update');
         window.location.href='paymentview.php?payment_id=<?php echo $payment_id; ?>';
         </script>
         <?php 
@@ -55,7 +55,7 @@ if(isset($_POST['approve']))
     $result3 = mysqli_query($con, "UPDATE payment SET status = '$status' WHERE payment_id = $payment_id ") or die(mysqli_error($con));
     ?>
     <script>
-    alert('Payment approved');
+    alert('Success to Update');
     window.location.href='paymentview.php?payment_id=<?php echo $payment_id; ?>';
     </script>
     <?php

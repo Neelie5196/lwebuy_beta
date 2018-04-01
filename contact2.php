f1943186f609131d7092ff4656422cf481ecebd5
Samuel Hii <wosamuelh.t.o@hotmail.my>
2018-04-01 13:38:32 +0800

    contact and user dashboard

---
+++ contact2.php
@@ -0,0 +1,103 @@
+<html>
+    <head>
+        <title>LWE Buy</title>
+
+        <meta name="viewport" content="width=device-width, initialscale=1.0"/>
+
+        <!-- Angularjs -->
+        <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.6.4/angular.min.js"></script>
+        
+        <!-- Bootstrap -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>    
+
+        <!--stylesheet-->
+        <link href="frameworks/css/style.css" rel="stylesheet"/>
+        <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+        <!--[if lt IE 9]>
+        <script src="js/html5shiv.js"></script>
+        <script src="js/respond.min.js"></script>
+        <![endif]-->
+        
+    </head>
+
+    <body class="linearbg">
+        <div class="row contact">
+            <div class="col-xs-12 col-md-8 col-lg-8 col-md-push-2 col-lg-push-2">
+                <h1 class="bigh1 center hidden-xs hidden-sm">Get In Touch With Us!</h3>
+                <h1 class="smh1 center hidden-md hidden-lg">Get In Touch With Us!</h3>
+                
+                <p class="center cap">Feel free to call one of our offices if you need further assistance!</p>
+                
+                <table class="contacttbl">
+                    <tr>
+                        <th class="purchasecol20">Country</th>
+                        <th class="purchasecol5">Address</th>
+                        <th class="purchasecol2">Contact No.</th>
+                        <th class="purchasecol2">Fax No.</th>
+                    </tr>
+                    
+                    <tr class="bodyrow">
+                        <td>Hong Kong</td>
+                        <td>Unit 9, 3/F, Hong Leong Industrial Complex, 4 Wang Kwong Road, Kowloon Bay, Kowloon, Hong Kong</td>
+                        <td>+852 3421 1122</td>
+                        <td>+852 3421 1134</td>
+                    </tr>
+                    
+                    <tr class="bodyrow">
+                        <td>Malaysia (Corporate Office)</td>
+                        <td>
+                            <p>Tower 3, Level 3A, Unit 7 (03-03A-07), UOA Business Park, No.1, Jalan Pengaturcara U1/51A, Seksyen U1, 40150 Shah Alam, Selangor Darul Ehsan. Malaysia</p>
+                            <a href="https://www.google.com/maps/place/Logistics+Worldwide+Express+(M)+Sdn+Bhd/@3.0849122,101.5840686,15z/data=!4m5!3m4!1s0x31cc4c4485dea883:0xd8713c20f8bf1f57!8m2!3d3.0859611!4d101.5867375?hl=en-AU" target="_blank">View on map</a>
+                        </td>
+                        <td>+603 8800 8838</td>
+                        <td>+603 8800 8839</td>
+                    </tr>
+                    
+                    <tr class="bodyrow">
+                        <td>Malaysia (LWE Logistics Hub)</td>
+                        <td>
+                            <p>No.4, (Block B) Lorong SS 13/6C, Subang Jaya Industrial Estate, 47500 Subang, Selangor Darul Ehsan, Malaysia</p>
+                            <a href="https://www.google.com/maps/place/LWE+Logistics+Hub/@3.0679443,101.5973846,17z/data=!3m1!4b1!4m5!3m4!1s0x0:0xdac46c3287b0a919!8m2!3d3.0679443!4d101.5995733?hl=en" target="_blank">View on map</a>
+                        </td>
+                        <td>+603 8800 8830</td>
+                        <td>+603 8800 8831</td>
+                    </tr>
+                    
+                    <tr class="bodyrow">
+                        <td>China</td>
+                        <td>7th, Second Road, Industrial Estate in Xin Mu Old Village, Ping Hu Town, Loggang District, Shenzhen, Guangdong, 518111, China</td>
+                        <td>
+                            <p>+86 755 2371 6515<br/>Ext 801</p>
+                            <p>+86 755 2371 6731</p>
+                        </td>
+                        <td></td>
+                    </tr>
+                    
+                    <tr class="bodyrow">
+                        <td>Taiwan</td>
+                        <td>台灣利威國際物流股份有限公司, Logistics Worldwide Express (TW) Co., Ltd, No.323, Sec. 2, Nanzhu Rd., Luzhu Dist., Taoyuan City 338, Taiwan</td>
+                        <td> +886 3 323 6081</td>
+                        <td>+886 3 323 6091</td>
+                    </tr>
+                    
+                    <tr class="bodyrow">
+                        <td>Singapore</td>
+                        <td>26 Kallang Avenue #06-00, Singapore 339417</td>
+                        <td>+65 6817 5500</td>
+                        <td></td>
+                    </tr>
+                    
+                    <tr class="bodyrow">
+                        <td>Korea</td>
+                        <td>ROOM 602, Bumhwa Building, 34 Namdaemun-Ro 1-Gil Jung-Gu</td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                </table>
+            </div>
+        </div>
+    </body>
+</html>
